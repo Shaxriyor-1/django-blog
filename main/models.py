df2d7366cbbb6ba4e687e6220169935f23e043fa
Shaxriyor-1 <shakhriyorbakhranov@gmail.com>
2022-12-14 01:26:33 +0300

    date added

--- main/models.py
+++ main/models.py
@@ -2,16 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Author(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    email = models.EmailField()
+    
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
-    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, default=None, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
     def snippet(self):
         return self.body[:50] + '...'
         
     
-class Author(models.Model):
-    firstname = models.CharField(max_length=200)
-    lastname = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
     

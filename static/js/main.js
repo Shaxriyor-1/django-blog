30844a4027c74147735b10234b9bf6673a71efbf
Olim2508 <rahmatovolim3@gmail.com>
2022-12-15 12:48:15 +0500

    fix modal

--- static/js/main.js
+++ static/js/main.js
@@ -8,5 +8,5 @@ deleteModalButtons.forEach(deleteModalButton => deleteModalButton.addEventListen
     const pk = deleteModalButton.getAttribute("data-pk")
     const title = deleteModalButton.getAttribute("data-title")
     deleteBlogBtn.href = `${window.location.origin}/blog_delete/${pk}/`
-    modalTitle.innerHTML += ` post "${title}"?`
+    modalTitle.innerHTML = `Do you really want to delete post "${title}"?`
 }))

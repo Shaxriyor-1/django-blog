1f98f29aa1e302665677abb3cda4c25ac2d9ac88
Olim2508 <rahmatovolim3@gmail.com>
2022-12-15 12:38:43 +0500

    added delete modal

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+console.log('hello')
+
+const deleteModalButtons = [...document.getElementsByClassName("del-modal-button")]
+const deleteBlogBtn = document.getElementById("delete-post-button")
+const modalTitle = document.getElementById("modal-title")
+
+deleteModalButtons.forEach(deleteModalButton => deleteModalButton.addEventListener("click", () => {
+    const pk = deleteModalButton.getAttribute("data-pk")
+    const title = deleteModalButton.getAttribute("data-title")
+    deleteBlogBtn.href = `${window.location.origin}/blog_delete/${pk}/`
+    modalTitle.innerHTML += ` post "${title}"?`
+}))

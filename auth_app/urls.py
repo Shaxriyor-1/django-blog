bd89c974ffd0c2836cc7deb627742d15e88de5b0
Shaxriyor-1 <shakhriyorbakhranov@gmail.com>
2022-12-18 18:42:31 +0300

    reset

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('password/', views.PasswordsChangeView.as_view(), name='password_change'),
     path('password_success/', views.password_change_success, name='password_success'),
+    path('password_reset/', views.password_reset, name='password_reset'),
     # path('dashboard/', views.dashboard, name='dashboard'),
     # path('signup/', views.signup, name='signup'),
 ]

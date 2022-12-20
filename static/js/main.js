2f97a26e2c11d61e71c2c1e5fe35d90f24beeb1f
Shaxriyor-1 <shakhriyorbakhranov@gmail.com>
2022-12-20 12:27:47 +0300

    url avatar

--- static/js/main.js
+++ static/js/main.js
@@ -10,3 +10,15 @@ deleteModalButtons.forEach(deleteModalButton => deleteModalButton.addEventListen
     deleteBlogBtn.href = `${window.location.origin}/blog_delete/${pk}/`
     modalTitle.innerHTML = `Do you really want to delete post "${title}"?`
 }))
+
+
+const avatarUrl = $("#submit-btn").attr("data-avatar-url")
+$("#avatar-input").parent().attr('id', 'avatar-p');
+$("#avatar-p").append(`<div class="mt-3">Current: <img 
+        class="rounded-circle img-fluid" 
+        style="width: 150px;" 
+        src='${avatarUrl}'/>
+        </div>`)
+
+
+        

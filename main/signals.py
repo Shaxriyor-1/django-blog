07c96ec413c28350c411ea878ceea9a952d20bad
Olim2508 <rahmatovolim3@gmail.com>
2022-12-20 13:43:07 +0500

    signals

--- main/signals.py
+++ main/signals.py
@@ -13,11 +13,11 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.save()
 
 
-@receiver(post_save, sender=User)
-def edit_profile(sender, instance, created, **kwargs):
-    print(sender, instance, created, kwargs, "SIGNAL")
-    if created:
-        user_profile = Profile(user=instance, firstname=instance.first_name, lastname=instance.last_name)
-        user_profile.save()
+# @receiver(post_save, sender=User)
+# def edit_profile(sender, instance, created, **kwargs):
+#     print(sender, instance, created, kwargs, "SIGNAL")
+#     if created:
+#         user_profile = Profile(user=instance, firstname=instance.first_name, lastname=instance.last_name)
+#         user_profile.save()
 
 
